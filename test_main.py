32f4fa7564a0aaba23fb04825ac07e98167562ff
IlyaMarvin <ilya.sereda2002@mail.ru>
2022-04-22 15:15:33 +0300

    Add some test functions

--- test_main.py
+++ test_main.py
@@ -13,3 +13,27 @@ def test_get_books():
     response = client.get("/api/v1/books/")
     assert response.status_code == 200
     assert type(response.json()) is list
+
+
+def test_get_one_book():
+    response = client.get("/api/v1/books/1")
+    assert response.status_code == 200
+    assert type(response.json()) is list
+
+    
+def test_get_users():
+    response = client.get("/api/v1/users/")
+    assert response.status_code == 200
+    assert type(response.json()) is list
+
+        
+def test_get_authors():
+    response = client.get("/api/v1/authors/")
+    assert response.status_code == 200
+    assert type(response.json()) is list
+
+        
+def test_get_transactions():
+    response = client.get("/api/v1/transactions/")
+    assert response.status_code == 200
+    assert type(response.json()) is list
