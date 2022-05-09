443c893856270dbf48e83d1190c1712cf8b11677
Ilya M <xresou@gmail.com>
2022-05-09 15:00:09 +0300

    Changed the database credentials

--- sql_app/database.py
+++ sql_app/database.py
@@ -2,7 +2,7 @@ from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
-SQLALCHEMY_DATABASE_URL = "postgresql://qtamtnwlzwammm:7435683321aa9ccfd1524a01b8aeb2e85d9f8014ad2fbab47d7fb68fe7701d66@ec2-99-80-170-190.eu-west-1.compute.amazonaws.com:5432/d8dqp0t5thoh68"
+SQLALCHEMY_DATABASE_URL = "postgresql://pofzbtuanlyhum:c016fe76d833bc5c1eeb66bb0c8e070105308cffce5b384101162e2e777bddf4@ec2-99-81-137-11.eu-west-1.compute.amazonaws.com:5432/d1eg01q97ttbua"
 
 engine = create_engine(SQLALCHEMY_DATABASE_URL)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
